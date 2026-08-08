49aa6550d44b191408a3dcf6b3b3eeea8aa0dbce
STEP3DLab <projects.step3d@gmail.com>
2026-08-08 10:29:05 +0300

    Add STEP_3D offline cache

---
+++ step3d/sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'step3d-v14';
+const ASSETS = ['./','index.html','styles.css','config.js','app.js','privacy.html','404.html','manifest.webmanifest','icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./404.html'))));
+});
